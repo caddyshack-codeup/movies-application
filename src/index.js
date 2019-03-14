be33bfcab461b67d9b31be37f9b240b3cfb1e4fe
jerlogan <jeremy.logan.web@gmail.com>
2019-03-14 18:23:11 -0500

    new data added

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 /**
  * es6 modules and imports
  */
-import sayHello from './hello';
-sayHello('World');
+// import sayHello from './hello';
+// sayHello('World');
 
 /**
  * require style imports
  */
 const {getMovies} = require('./api.js');
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+let htmlString = '';
+
+  getMovies().then((movies) => {
+    movies.forEach(({title, rating}) => {
+      htmlString += (`<div class="each-movie"><h1>${title}</h1><p>${rating}</p></div>`)
+    });
+
+
+
+console.log(htmlString);
+
+
+    for (var i = 0; i < htmlString.length; i++) {
+      $('.movie-container').html(htmlString);
+
+    };
+  }).catch((error) => {
+    alert('Oh no! Something went wrong.\nCheck the console for details.')
+    console.log(error);
   });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
-});
